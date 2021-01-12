96b11094fc01d55ee1d0d94ad1954cc0e5a6f9fd
upluke <lukeforadream@gmail.com>
2021-01-11 23:44:36 -0800

    init-data

---
+++ royal-thai/src/data/data.js
@@ -0,0 +1,40 @@
+export const MENU_DATA =[
+    {
+        id:0,
+        category:'Appetizers',
+        dishes:[
+            {
+                id:0,
+                name:'Sampler for 2',
+                price: 13.35,
+                description: 'Sampler plate of Fried shumai, crab Rangoon, pot sticker and chicken egg rolls.'
+            },
+            {
+                id:1,
+                name:'Fresh Spring Rolls  ',
+                price: 5.95,
+                description:'Fresh spring rolls stuffed with tofu, egg, cucumber and bean sprout topped with tamarind sauce.'
+            }
+        ]
+
+    },
+    {
+        id:1,
+        category:'Soups',
+        dishes:[
+            {
+                id:0,
+                name:'Tom Yum (Large)',
+                price: 9.95,
+                description: 'Sampler plate of Fried shumai, crab Rangoon, pot sticker and chicken egg rolls.'
+            },
+            {
+                id:1,
+                name:'Fresh Spring Rolls  ',
+                price: 5.95,
+                description:'Famous Thai hot and sour soup with straw mushroom, fresh mushroom, lemongrass and lemon leaves. Choice of chicken, tofu or vegetable.'
+            }
+        ]
+
+    }
+]
