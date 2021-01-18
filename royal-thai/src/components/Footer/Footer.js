4f31d613910d8e01832d4d2f9aaebffd437b0b55
upluke <lukeforadream@gmail.com>
2021-01-18 00:42:25 -0800

    styleOverides

--- royal-thai/src/components/Footer/Footer.js
+++ royal-thai/src/components/Footer/Footer.js
@@ -2,18 +2,28 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
- 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
+const theme = createMuiTheme({ // style overides at the theme level
+  props: {
+    MuiContainer: { // MuiContainer is the original component name.
+      maxWidth: false
+    }
+  }
+});
+ 
 
 export default function Footer() {
   return (
+    <ThemeProvider theme={theme}>
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth='xl'  style={{ backgroundColor: '#646C79', height: '30vh'   }} >
+      <Container   style={{ backgroundColor: '#646C79', height: '30vh',maxWidth:false   }} >
             <Typography align="center" variant="h2">Test</Typography>
         
       </Container>
     </React.Fragment>
+    </ThemeProvider>
   );
 }
 
--- royal-thai/src/components/Hero/Hero.js
+++ royal-thai/src/components/Hero/Hero.js
@@ -3,16 +3,25 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
-
+const theme = createMuiTheme({ // style overides at the theme level
+  props: {
+    MuiContainer: { // MuiContainer is the original component name.
+      maxWidth: false
+    }
+  }
+});
 export default function Hero() {
   return (
+    <ThemeProvider theme={theme}>
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth='xl'  style={{ backgroundColor: '#FB9039', height: '50vh'   }} >
+      <Container    style={{ backgroundColor: '#FB9039', height: '50vh' }} >
             <Typography align="center" variant="h2">Test</Typography>
             <Button  color="inherit">Login</Button>
       </Container>
     </React.Fragment>
+    </ThemeProvider>
   );
 }
