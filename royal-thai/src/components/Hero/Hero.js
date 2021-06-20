73833a6bc0cba870ea61ea4f0aaa6574816d369d
upluke <lukeforadream@gmail.com>
2021-06-19 23:54:31 -0700

    heroHs

--- royal-thai/src/components/Hero/Hero.js
+++ royal-thai/src/components/Hero/Hero.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({ // style overides at the theme level
   props: {
@@ -12,13 +13,24 @@ const theme = createMuiTheme({ // style overides at the theme level
     }
   }
 });
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding:'10%',
+    backgroundColor: '#FB9039',
+    height:'50vh'
+  },
+ 
+}));
+
 export default function Hero() {
+  const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
     <React.Fragment>
       <CssBaseline />
-      <Container    style={{ backgroundColor: '#FB9039', height: '50vh' }} >
-            <Typography align="center" variant="h2">Test</Typography>
+      <Container className={classes.container}   >
+            <Typography align="center" variant="h5">Welcome to</Typography>
+            <Typography align="center" variant="h1">Royal Thai Restaurant</Typography>
             <Button  color="inherit">Login</Button>
       </Container>
     </React.Fragment>
